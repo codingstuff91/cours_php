41a909b019227d1eb20888aa12b0ae2921a6f8fc
Matthieu MARTIN <codingstuff91@gmail.com>
2020-11-18 15:27:50 +0100

    Ajout notions de bases utilisation de variables et fonctions

---
+++ fonctions.php
@@ -0,0 +1,6 @@
+<?php
+
+// Creation de la fonction qui va se charger d'afficher le prenom avec le nom
+function afficherNomEtPrenom($prenom, $nom){
+    echo $prenom . " " . $nom;
+}
--- variables.php
+++ variables.php
@@ -1,2 +1,39 @@
 <?php
+// On appelle le fichier fonctions.php à l'interieur de celui ci
+include('fonctions.php');
 
+// Variables (commencer par le signe $)
+$texte = "Bienvenue sur mon premier site";
+$nombre = 1;
+$nombre_a_virgule = 1.5;
+
+// Concaténation de texte et de variable (par ex : Bienvenue sur mon site Mallé)
+$prenom = "Mallé";
+$texte_a_afficher = "Bienvenue sur mon site " . $prenom;
+echo "Bienvenue sur mon site " . $prenom;
+
+// Ajout de code HTML dans une instruction PHP
+echo "<p>Je suis un paragraphe</p>";
+
+// Variables de type tableau - ARRAY (commencent à l'indice 0)
+// 1. tableaux indéxés
+// 2. tableaux associatifs
+
+// 1. tableau indexé (par ex : un tableau contenant des fruits rouges): 
+$tableau_index = [ "fraise", "framboise", "cassis"];
+
+// si on veut afficher la valeur "framboise" il faudra écrire : 
+echo $tableau_index[0];
+
+// 2. Tableau associatifs, regroupant plusieurs éléments via un systeme de clé et de valeur.
+$tableau_assoc = [ "prenom" => "Matthieu", "nom" => "Martin", "age" => 38 ];
+
+// Si on veut afficher le prénom avec le nom de famille : 
+echo $tableau_assoc["prenom"] . $tableau_assoc["nom"];
+
+// Debugguer un tableau (indexé ou associatif) il faut utiliser la fonction var_dump()
+var_dump($tableau_assoc);
+
+// Ex utilisation de fonction : 
+// On passe par la fonction dédiée pour afficher le prenom et le nom
+afficherNomEtPrenom($tableau_assoc["prenom"], $tableau_assoc["nom"]);
